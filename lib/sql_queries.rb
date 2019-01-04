ef564a6691aac29e419d99f1e69cefe17f9fc3bc
Ryan Brady <ryanbrady369@gmail.com>
2019-01-04 23:50:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -54,7 +54,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT
      users.name,
-     SUM(pledges.amount)
+     SUM(pledges.amount) AS total
    FROM
      users
      INNER JOIN
@@ -63,8 +63,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
      GROUP BY
        users.name
      ORDER BY
-       pledges.amount,
-       users.name;"
+       pledges.amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
