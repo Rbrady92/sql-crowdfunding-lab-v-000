7b4acaa355160ef07110f89c8c3027e22f7c705e
Ryan Brady <ryanbrady369@gmail.com>
2019-01-05 00:27:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,9 +36,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
  "SELECT
     projects.title,
-    (
-      SUM(pledges.amount) - projects.funding_goal
-    )
+    (SUM(pledges.amount) - projects.funding_goal)
     AS over_goal
   FROM
     projects
